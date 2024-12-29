b3c1423e042d73203b9d11f2f8a2d81e3bd26057
mahmoud ismail <100169351+mahm0udismail@users.noreply.github.com>
2024-12-29 17:57:44 +0200

    Time: 104 ms, Memory: 12.8 MB - LeetHub

---
+++ 1100-103-2574-left-and-right-sum-differences/1100-103-2574-left-and-right-sum-differences.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def leftRightDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = []
+        for i in range(len(nums)):
+            answer.append(abs(sum(nums[:i]) - sum(nums[i+1:])))
+        return answer
